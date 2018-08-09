03ee0eda0b6a2f91c5d92ad15280f79ffc2a157e
kazinur <kazinur@mybihs.org>
2018-08-09 20:09:02 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,21 +1,21 @@
 class Dog 
-  def initialize (name, name1, name2)
-       @name= name 
-       @name1=name1
-       @name2= name2
-  end 
+  # def initialize (name, name1, name2)
+  #     @name= name 
+  #     @name1=name1
+  #     @name2= name2
+  # end 
      
-     def name 
-       @name
-     end 
+  #   def name 
+  #     @name
+  #   end 
      
-     def name1
-       @name1
-     end 
+  #   def name1
+  #     @name1
+  #   end 
      
-     def name2 
-       @name2
-     end 
+  #   def name2 
+  #     @name2
+  #   end 
    end 
    
    dogs_name= Dog.new("fido","snoopy", "lassie")
